bbc95a84edd673f55515084628bdd1ef9c92505c
Dasha <dashan340@gmail.com>
2022-05-10 19:45:39 +0300

    Пятый коммит

--- Kontrol/Program.cs
+++ Kontrol/Program.cs
@@ -1,6 +1,14 @@
 ﻿void Text(string[] args)
 {
     string[] textGiven = {"hello","2","world",":-)"};
+        Console.Write("[");
+    List<int> lengths = textGiven.Select(text => text.Length).ToList();
+    foreach (int length in lengths)
+    
+     if (length  <=3)
+        {
+        Console.Write($"{textGiven[length]};");
+        }
   Console.Write("]");
 }
 
